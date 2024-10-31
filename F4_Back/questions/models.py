71b32efd2d7741a8f8aeb18409eeaf0ed995f0be
LEE <sw09112@yonsei.ac.kr>
2024-10-31 16:36:19 +0900

    models

---
+++ F4_Back/questions/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=255)
+    option_a = models.CharField(max_length=100)
+    option_b = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.question_text
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answer')
+    user_name = models.CharField(max_length=50)
+    selected_option = models.CharField(max_length=1, choices=[('A', 'Option A'),('B', 'Option B')])
+
+    def __str__(self):
+        return f"{self.user_name} - {self.question} - {self.selected_option}"
--- F4_Back/teams/models.py
+++ F4_Back/teams/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    member_count = models.CharField(max_length=100)
+
+class Profile(models.Model):
+    PARTICIPATION_CHOICES = [
+        ('frontend', '프론트엔드'),
+        ('backend', '백엔드'),
+        ('planning', '기획'),
+        ('design', '디자인'),
+    ]
+    user_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    mbti = models.CharField(max_length=4)
+    affiliations = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    participation_field = models.CharField(
+        max_length=20,
+        choices=PARTICIPATION_CHOICES
+    )
+
+    def __str__(self):
+        return self.user_name
