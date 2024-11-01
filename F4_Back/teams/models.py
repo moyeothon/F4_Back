69bd5bb52a90e74bc0f42955125f045bea465648
brightalot <gamja9151@hanyang.ac.kr>
2024-11-02 00:32:35 +0900

    refact : team_id 필드 int 에서 uuid 로 변경

--- F4_Back/teams/models.py
+++ F4_Back/teams/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+import uuid
 
 class Team(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     member_count = models.CharField(max_length=100)
 
 class Profile(models.Model):
